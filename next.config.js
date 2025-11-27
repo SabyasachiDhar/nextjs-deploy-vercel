7a564fe1f7d7deb74d8aa2fde0919f273373053a
SabyasachiDhar <SabyasachiDhar@users.noreply.github.com>
2025-11-27 17:26:48 +0530

    check updates 3

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = (phase) => {
     return {
       env: {
         mongodb_username: encodeURIComponent("dharsabyasachivi_db_user"),
-        mongodb_password: encodeURIComponent("s4y1e9PXfnFhWkxv"),
+        mongodb_password: encodeURIComponent("ey2H6HdN5bHHZhA8"),
         mongodb_clustername: 'cluster0',
         mongodb_database: 'my-site-dev',
       },
@@ -15,7 +15,7 @@ module.exports = (phase) => {
   return {
     env: {
       mongodb_username: encodeURIComponent("dharsabyasachivi_db_user"),
-      mongodb_password: encodeURIComponent("s4y1e9PXfnFhWkxv"),
+      mongodb_password: encodeURIComponent("ey2H6HdN5bHHZhA8"),
       mongodb_clustername: 'cluster0',
       mongodb_database: 'my-site',
     },
